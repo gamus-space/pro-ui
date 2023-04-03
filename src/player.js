ce918e66af8a5bb98b9a5b301ddf8d7286bfab48
Krzysztof Kostałkowicz <12763939+krzykos@users.noreply.github.com>
2023-04-03 21:37:25 +0200

    Create player UI & API

---
+++ src/player.js
@@ -0,0 +1,97 @@
+'use strict';
+
+import { readFlacMeta, flacGainValue } from './utils.js'
+
+export class Player {
+  // audioContext
+  // audio
+  // loading
+  // handlers
+
+  constructor() {
+    this.loading = false;
+    this.handlers = {};
+    this.initialize(new Audio());
+
+    ['play', 'pause'].forEach(method => {
+      this[method] = () => { this.audio[method](); };
+    });
+    ['canplay', 'play', 'pause', 'ended', 'timeupdate'].forEach(event => {
+      this.audio.addEventListener(event, () => { this.handlers[event]?.(); });
+    });
+    ['loop', 'volume', 'duration', 'currentTime'].forEach(field => {
+      Object.defineProperty(this, field, {
+        get() { return this.audio[field]; },
+        set(v) { this.audio[field] = v; },
+      });
+    });
+
+    Object.seal(this);
+  }
+
+  initialize(audio) {
+    this.audio = audio;
+    this.audioContext = new AudioContext();
+
+    const audioInput = this.audioContext.createMediaElementSource(this.audio);
+    const replayGainNode = this.audioContext.createGain();
+    audioInput.connect(replayGainNode);
+    const splitter = this.audioContext.createChannelSplitter(2);
+    replayGainNode.connect(splitter);
+    const gainL1 = this.audioContext.createGain();
+    const gainL2 = this.audioContext.createGain();
+    const gainR1 = this.audioContext.createGain();
+    const gainR2 = this.audioContext.createGain();
+    splitter.connect(gainL1, 0);
+    splitter.connect(gainL2, 0);
+    splitter.connect(gainR1, 1);
+    splitter.connect(gainR2, 1);
+    const merger = this.audioContext.createChannelMerger(2);
+    gainL1.connect(merger, 0, 0);
+    gainR1.connect(merger, 0, 0);
+    gainL2.connect(merger, 0, 1);
+    gainR2.connect(merger, 0, 1);
+    merger.connect(this.audioContext.destination);
+
+    let replayGain = 0;
+    Object.defineProperty(this, 'replayGain', {
+      get() { return replayGain; },
+      set(v) {
+        replayGain = v;
+        replayGainNode.gain.value = Math.pow(10, replayGain/20);
+      },
+    });
+
+    let stereo = 1;
+    Object.defineProperty(this, 'stereo', {
+      get() { return stereo; },
+      set(v) {
+        stereo = v;
+        gainL1.gain.value = (1+stereo)/2;
+        gainL2.gain.value = (1-stereo)/2;
+        gainR1.gain.value = (1-stereo)/2;
+        gainR2.gain.value = (1+stereo)/2;
+      },
+    });
+    this.stereo = stereo;
+  }
+
+  on(event, handler) {
+    this.handlers[event] = handler;
+  }
+
+  load(href, play) {
+    if (this.loading) return;
+    this.loading = true;
+    const url = `media/${href}`;
+    fetch(url, { headers: { Range: 'bytes=0-1279' } }).then(response => response.arrayBuffer()).then(header => {
+      this.replayGain = flacGainValue(readFlacMeta(header, "replaygain_album_gain"));
+      console.log({ replayGain: this.replayGain });
+      this.audio.src = url;
+      if (play)
+        this.audio.play();
+    }).finally(() => {
+      this.loading = false;
+    });
+  }
+}
--- src/script.js
+++ src/script.js
@@ -1,10 +1,7 @@
 'use strict';
 
 import { loadDb } from './db.js';
-import { readFlacMeta, flacGainValue } from './utils.js'
-
-let replayGain = 0;
-let stereoSeparation = 1;
+import { Player } from './player.js';
 
 let db = [];
 loadDb().then(data => {
@@ -21,64 +18,86 @@ $('#library').on('click', event => {
   play(event.target.attributes['data-href'].value);
 });
 
-const htmlPlayer = $('#htmlPlayer');
-console.log(htmlPlayer[0]);
-htmlPlayer.on('canplay', () => {
-  console.log('canplay');
-});
-htmlPlayer.on('play', () => {
-  console.log('play');
-});
-htmlPlayer.on('ended', () => {
-  console.log('ended');
-});
+class Controls {
+  constructor() {
+    this._canplay = false;
+    this._paused = true;
+    this._loop = false;
+    this._duration = 0;
+    this._position = 0;
+    this.seeking = false;
+    Object.seal(this);
+  }
+  get canplay() {
+    return this._canplay;
+  }
+  set canplay(v) {
+    this._canplay = v;
+    $('.play').button('option', 'disabled', !this._canplay);
+  }
+  get paused() {
+    return this._paused;
+  }
+  set paused(v) {
+    this._paused = v;
+    $('.play .ui-icon').toggleClass('ui-icon-play', this._paused);
+    $('.play .ui-icon').toggleClass('ui-icon-pause', !this._paused);
+  }
+  get loop() {
+    return this._loop;
+  }
+  set loop(v) {
+    this._loop = v;
+  }
+  get duration() {
+    return this._duration;
+  }
+  set duration(v) {
+    this._duration = v;
+    $('.duration').text(this.time(v));
+    $('.controls .seek').slider('option', 'max', this._duration);
+  }
+  get position() {
+    return this._position;
+  }
+  set position(v) {
+    this._position = v;
+    $('.position').text(this.time(v));
+    if (!this.seeking)
+      $('.controls .seek').slider('value', this._position);
+  }
 
-const audioContext = new AudioContext();
-const audioInput = audioContext.createMediaElementSource(htmlPlayer[0]);
-const replayGainNode = audioContext.createGain();
-audioInput.connect(replayGainNode);
-const splitter = audioContext.createChannelSplitter(2);
-replayGainNode.connect(splitter);
-const gainL1 = audioContext.createGain();
-const gainL2 = audioContext.createGain();
-const gainR1 = audioContext.createGain();
-const gainR2 = audioContext.createGain();
-splitter.connect(gainL1, 0);
-splitter.connect(gainL2, 0);
-splitter.connect(gainR1, 1);
-splitter.connect(gainR2, 1);
-const merger = audioContext.createChannelMerger(2);
-gainL1.connect(merger, 0, 0);
-gainR1.connect(merger, 0, 0);
-gainL2.connect(merger, 0, 1);
-gainR2.connect(merger, 0, 1);
-merger.connect(audioContext.destination);
-updateNodes();
-function updateNodes() {
-  replayGainNode.gain.value = Math.pow(10, replayGain/20);
-  gainL1.gain.value = (1+stereoSeparation)/2;
-  gainL2.gain.value = (1-stereoSeparation)/2;
-  gainR1.gain.value = (1-stereoSeparation)/2;
-  gainR2.gain.value = (1+stereoSeparation)/2;
+  time(t) {
+    t = Math.floor(t)
+    const sec = t % 60;
+    const min = Math.floor(t/60);
+    return `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}`;
+  }
 }
 
-let loading = false;
+const player = new Player();
+const controls = new Controls();
+player.on('canplay', () => {
+  controls.canplay = true;
+  controls.duration = player.duration;
+});
+player.on('play', () => {
+  controls.paused = false;
+});
+player.on('pause', () => {
+  controls.paused = true;
+});
+player.on('ended', () => {
+});
+player.on('timeupdate', (e) => {
+  controls.position = player.currentTime;
+});
+
 function play(href) {
-  if (loading) return;
-  loading = true;
+  player.load(href, true);
   console.log('play', href);
   const track = db.find(track => track.href === href);
   $('#title').text(track.title);
-  const url = `media/${href}`;
-  fetch(url, { headers: { Range: 'bytes=0-1279' } }).then(response => response.arrayBuffer()).then(header => {
-    replayGain = flacGainValue(readFlacMeta(header, "replaygain_album_gain"));
-    console.log({ replayGain });
-    updateNodes();
-    htmlPlayer[0].src = url;
-    htmlPlayer[0].play();
-  }).finally(() => {
-    loading = false;
-  });
 }
 
 $('#playerDialog').dialog({
@@ -101,3 +120,75 @@ $('#libraryDialog').dialog({
   },
 });
 $('.ui-dialog-titlebar button').blur();
+
+$('input[type=checkbox]').checkboxradio({
+  icon: false,
+});
+$('.play').button({ disabled: true });
+$('.controls .seek').slider({
+  value: 0,
+  min: 0,
+  max: 0,
+  step: 1,
+  orientation: "horizontal",
+  range: "min",
+  animate: "fast",
+});
+$('.controls .volume').slider({
+  value: 1,
+  min: 0,
+  max: 1,
+  step: 0.01,
+  orientation: "horizontal",
+  range: "min",
+  animate: "fast",
+});
+$('.controls .stereo').slider({
+  value: 1,
+  min: -1,
+  max: 1,
+  step: 0.01,
+  orientation: "horizontal",
+  animate: "fast",
+});
+
+$('.volume-min').click(() => {
+  $('.controls .volume').slider('value', 0);
+});
+$('.volume-max').click(() => {
+  $('.controls .volume').slider('value', 1);
+});
+$('.stereo-rev').click(() => {
+  $('.controls .stereo').slider('value', -1);
+});
+$('.mono').click(() => {
+  $('.controls .stereo').slider('value', 0);
+});
+$('.stereo-full').click(() => {
+  $('.controls .stereo').slider('value', 1);
+});
+
+$('.play').click(() => {
+  if (controls.paused) player.play();
+  else player.pause();
+});
+$('.loop').change(() => {
+  controls.loop = !controls.loop;
+  player.loop = controls.loop;
+});
+$('.controls .volume').on('slide', (e, { value }) => {
+  player.volume = value;
+});
+$('.controls .stereo').on('slide', (e, { value }) => {
+  player.stereo = value;
+});
+$('.controls .seek').on('slidestart', () => {
+  controls.seeking = true;
+});
+$('.controls .seek').on('slidestop', () => {
+  controls.seeking = false;
+});
+$('.controls .seek').on('slidechange', (e, { value }) => {
+  if (!e.originalEvent) return;
+  player.currentTime = value;
+});
